1d14b9d897f7a785f5b2497e4ec68f3e1e3b6c29
BoipeloMkk <mokakaleboipelo@gmail.com>
2024-07-26 10:35:36 +0200

    updated some of the container contents

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
 
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = "Tuesday 14:49";
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
