bde929c51f02ea13d3eafb8b93d64e48f948bab7
BoipeloMkk <mokakaleboipelo@gmail.com>
2024-07-26 16:44:21 +0200

    Improved the header details and weather data

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ function refreshWeather(response) {
   let windspeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  icon.innerHTML = `<img
+                src="${response.data.condition.icon_url}"
+                class="weather-app-icon"
+              />`;
   cityElement.innerHTML = response.data.city;
-
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
